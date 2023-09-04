c9db4ed0b528854e296969eecfd1c92b5dc5e085
mikolajmizera <mikolajmizera@gmail.com>
2023-09-04 21:28:32 +0200

    fix/prompt_tests

--- tests/unit/test_prompt.py
+++ tests/unit/test_prompt.py
@@ -10,10 +10,10 @@ from financegpt.prompting.prompt import Prompt
 def ohlc_template():
     return (
         "What is the performance of {datapoint_symbol} on {datapoint_timestamp}"
-        + " with interval {datapoint_interval}?\nThe performance of "
-        + "{datapoint_symbol} on {datapoint_timestamp} ({datapoint_interval}) "
-        + "is {datapoint_open} {datapoint_high} {datapoint_low} "
-        + "{datapoint_close} {datapoint_volume}"
+        + " with interval {datapoint_interval}?\nThe performance of"
+        + " {datapoint_symbol} on {datapoint_timestamp} ({datapoint_interval})"
+        + " is {datapoint_open} {datapoint_high} {datapoint_low}"
+        + " {datapoint_close} {datapoint_volume}"
     )
 
 
@@ -21,13 +21,32 @@ def ohlc_template():
 def text_template():
     return (
         "What is the news for {datapoint_symbol} on {datapoint_timestamp}"
-        + " with interval {datapoint_interval}?\nThe news for "
-        + "{datapoint_symbol} on {datapoint_timestamp} ({datapoint_interval}) "
-        + "is {datapoint_text}"
+        + " with interval {datapoint_interval}?\nThe news for"
+        + " {datapoint_symbol} on {datapoint_timestamp} ({datapoint_interval})"
+        + " is {datapoint_text}"
     )
 
 
-def test_ohlc_template_parsing(ohlc_data_point: OhlcDataPoint, ohlc_template: str):
+@pytest.fixture
+def expected_ohlc_prompt():
+    return (
+        "What is the performance of AAPL on 2021-01-01 00:00:00 with interval W?"
+        + "\nThe performance of AAPL on 2021-01-01 00:00:00 (W) is 1.0 2.0 0.5"
+        + " 1.5 10000"
+    )
+
+
+@pytest.fixture
+def expected_text_prompt():
+    return (
+        "What is the news for AAPL on 2021-01-01 00:00:00 with interval W?"
+        + "\nThe news for AAPL on 2021-01-01 00:00:00 (W) is This is a test"
+    )
+
+
+def test_ohlc_template_parsing(
+    ohlc_data_point: OhlcDataPoint, ohlc_template: str, expected_ohlc_prompt: str
+):
     example_prompt = PromptTemplate(
         input_variables=[
             "datapoint_symbol",
@@ -45,14 +64,12 @@ def test_ohlc_template_parsing(ohlc_data_point: OhlcDataPoint, ohlc_template: st
     prompt_str = example_prompt.format(
         **ohlc_data_point.dict_for_template(prefix="datapoint_")
     )
-    assert (
-        prompt_str
-        == """What is the performance of AAPL on 2021-01-01 00:00:00 with interval W?
-    The performance of AAPL on 2021-01-01 00:00:00 (W) is 1.0 2.0 0.5 1.5 10000"""
-    )
+    assert prompt_str == expected_ohlc_prompt
 
 
-def test_text_template_parsing(text_data_point: TextDataPoint, text_template: str):
+def test_text_template_parsing(
+    text_data_point: TextDataPoint, text_template: str, expected_text_prompt: str
+):
     example_prompt = PromptTemplate(
         input_variables=[
             "datapoint_symbol",
@@ -66,14 +83,12 @@ def test_text_template_parsing(text_data_point: TextDataPoint, text_template: st
     prompt_str = example_prompt.format(
         **text_data_point.dict_for_template(prefix="datapoint_")
     )
-    assert (
-        prompt_str
-        == """What is the news for AAPL on 2021-01-01 00:00:00 with interval W?
-    The news for AAPL on 2021-01-01 00:00:00 (W) is This is a test"""
-    )
+    assert prompt_str == expected_text_prompt
 
 
-def test_create_prompt_ohlc(ohlc_data_point: OhlcDataPoint, ohlc_template: str):
+def test_create_prompt_ohlc(
+    ohlc_data_point: OhlcDataPoint, ohlc_template: str, expected_ohlc_prompt: str
+):
     prompt = Prompt(
         template=PromptTemplate(
             input_variables=list(ohlc_data_point.dict_for_template().keys()),
@@ -82,14 +97,12 @@ def test_create_prompt_ohlc(ohlc_data_point: OhlcDataPoint, ohlc_template: str):
         data_points=[ohlc_data_point],
     )
     prompt_str = prompt.format_prompt()
-    assert (
-        prompt_str
-        == """What is the performance of AAPL on 2021-01-01 00:00:00 with interval W?
-    The performance of AAPL on 2021-01-01 00:00:00 (W) is 1.0 2.0 0.5 1.5 10000"""
-    )
+    assert prompt_str == expected_ohlc_prompt
 
 
-def test_create_prompt_text(text_data_point: TextDataPoint, text_template: str):
+def test_create_prompt_text(
+    text_data_point: TextDataPoint, text_template: str, expected_text_prompt: str
+):
     prompt = Prompt(
         template=PromptTemplate(
             input_variables=list(text_data_point.dict_for_template().keys()),
@@ -98,15 +111,11 @@ def test_create_prompt_text(text_data_point: TextDataPoint, text_template: str):
         data_points=[text_data_point],
     )
     prompt_str = prompt.format_prompt()
-    assert (
-        prompt_str
-        == """What is the news for AAPL on 2021-01-01 00:00:00 with interval W?
-    The news for AAPL on 2021-01-01 00:00:00 (W) is This is a test"""
-    )
+    assert prompt_str == expected_text_prompt
 
 
 def test_create_prompt_ohlc_multiple(
-    ohlc_data_point: OhlcDataPoint, ohlc_template: str
+    ohlc_data_point: OhlcDataPoint, ohlc_template: str, expected_ohlc_prompt: str
 ):
     prompt = Prompt(
         template=PromptTemplate(
@@ -116,17 +125,11 @@ def test_create_prompt_ohlc_multiple(
         data_points=[ohlc_data_point, ohlc_data_point],
     )
     prompt_str = prompt.format_prompt()
-    assert (
-        prompt_str
-        == """What is the performance of AAPL on 2021-01-01 00:00:00 with interval W?
-    The performance of AAPL on 2021-01-01 00:00:00 (W) is 1.0 2.0 0.5 1.5 10000
-What is the performance of AAPL on 2021-01-01 00:00:00 with interval W?
-    The performance of AAPL on 2021-01-01 00:00:00 (W) is 1.0 2.0 0.5 1.5 10000"""
-    )
+    assert prompt_str == (expected_ohlc_prompt + "\n" + expected_ohlc_prompt)
 
 
 def test_create_prompt_text_multiple(
-    text_data_point: TextDataPoint, text_template: str
+    text_data_point: TextDataPoint, text_template: str, expected_text_prompt: str
 ):
     prompt = Prompt(
         template=PromptTemplate(
@@ -136,13 +139,7 @@ def test_create_prompt_text_multiple(
         data_points=[text_data_point, text_data_point],
     )
     prompt_str = prompt.format_prompt()
-    assert (
-        prompt_str
-        == """What is the news for AAPL on 2021-01-01 00:00:00 with interval W?
-    The news for AAPL on 2021-01-01 00:00:00 (W) is This is a test
-What is the news for AAPL on 2021-01-01 00:00:00 with interval W?
-    The news for AAPL on 2021-01-01 00:00:00 (W) is This is a test"""
-    )
+    assert prompt_str == (expected_text_prompt + "\n" + expected_text_prompt)
 
 
 def test_create_prompt_invalid_data_type(
