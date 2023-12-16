c8dfeedfa57fa6b6c9ac6337f9ef7d08979e7c67
mikolajmizera <mikolajmizera@gmail.com>
2023-12-16 22:26:48 +0100

    add openai api key to .env

--- tests/unit/test_llm_chain.py
+++ tests/unit/test_llm_chain.py
@@ -4,4 +4,4 @@ from financegpt.llm.chain import LLMChainInterfaceFactory
 def test_create_llm_chain():
     llm_chain = LLMChainInterfaceFactory.create_llm_chain("gpt-3.5-turbo-instruct")
     assert hasattr(llm_chain._chain.first, "model_name")
-    assert llm_chain._chain.first.model_name == "gpt-3.5-turbo-instruct"
+    assert llm_chain._chain.first.model_name == "gpt-3.5-turbo-instruct"  # type: ignore
